67c2cb022fbdd7842a2332902832b8c8cbd42888
abrarasif11 <faasif929@gmail.com>
2022-09-03 16:17:09 +0600

    news-sections-added

--- news.js
+++ news.js
@@ -21,5 +21,40 @@ const displayCatagories = (catagories) => {
         allCatagories.appendChild(catagoryDiv);
     })
 }
-
+// News section //
+const loadNews = async (id) => {
+    const url = `
+      https://openapi.programming-hero.com/api/news/category/0${id}
+      `;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayNewses(data.data);
+  };
+  
+  // Display News//
+  const displayNewses = (newses) => {
+    const newsContainer = document.getElementById("news-container");
+    newsContainer.innerHTML = "";
+  
+    newses.forEach((news) => {
+      console.log(news);
+      const newDiv = document.createElement("div");
+      newDiv.classList.add("col");
+      newDiv.innerHTML = `
+            <div class="card">
+                          <img src="${
+                            news.thumbnail_url
+                          }" class="card-img-top h-50" alt="...">
+                          <div class="card-body">
+                              <h5 class="card-title">${news.title}</h5>
+                              <p class="card-text">${news.details.slice(
+                                0,
+                                100
+                              )}...</p>
+                          </div>
+                      </div>
+      `;
+      newsContainer.appendChild(newDiv);
+    });
+  };
 loadCatagories();
