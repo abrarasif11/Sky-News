c472dfc7d3c50d708dbf97fdc4303db6499be0ad
abrarasif11 <faasif929@gmail.com>
2022-09-03 19:52:43 +0600

    add-news-author-info

--- news.js
+++ news.js
@@ -46,12 +46,19 @@ const loadNews = async (id) => {
                             news.thumbnail_url
                           }" class="card-img-top h-50" alt="...">
                           <div class="card-body">
-                              <h5 class="card-title">${news.title}</h5>
-                              <p class="card-text">${news.details.slice(
+                              <h5 style="font-family: 'Poppins', sans-serif;" class="card-title">${news.title}</h5>
+                              <p style="font-family: 'Poppins', sans-serif;" class="card-text">${news.details.slice(
                                 0,
                                 100
                               )}...</p>
-                          </div>
+                              <div class="d-flex align-items-start">
+                              <h6 style="font-family: 'Poppins', sans-serif;">${news.author.name ? news.author.name  : 'No Name Found'}</h6>
+                              </div>
+                              <img class="w-25 h-25 ps-2 rounded-circle" src="${news.author.img}" alt="">
+                              <p style="font-family: 'Poppins', sans-serif;" class="pt-3">${news.author.published_date ? news.author.published_date  : 'No Date Found'}</p>
+                              <p <i class="fa-regular fa-eye"></i> ${news.total_view ? news.total_view  : '0'}</p>
+                              </div>                            
+                              <button style="font-family: 'Poppins', sans-serif; background-color: #5D5FEF; color : white;" class="btn btn-" type="button">Details News</button> 
                       </div>
       `;
       newsContainer.appendChild(newDiv);
